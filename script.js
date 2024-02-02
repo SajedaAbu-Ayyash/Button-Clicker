0d74a4141edbf1fcf6beb70014883ec573fd8eb6
Sajeda Abu Ayyash <sajedaabuayyash8@gmail.com>
2024-02-02 23:33:12 +0200

    This is about click event

---
+++ script.js
@@ -0,0 +1,26 @@
+function logout(element){
+  if (element.innerText == "Login") {
+    element.innerText = "Logout"
+  } 
+  else{
+    element.innerText = "Login";
+  }
+}
+
+
+function remove(element){
+    element.remove();
+}
+
+/*
+Desc: This function will do ...
+Date:
+Developer Name: Nasri Ladaa
+Input:
+Outputs:
+
+
+*/
+function alertBtn() {
+   alert("ninja was liked");
+}
